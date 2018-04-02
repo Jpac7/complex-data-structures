e81335ec81758a62766fbaa2966c209746dae93e
Jpac <jose.costa@tradingeconomics.com>
2018-04-02 01:10:49 +0100

    testing avl balanced tree - O(log n)

---
+++ avlTree/App.py
@@ -0,0 +1,33 @@
+from avlTree.AVLTree import AVLTree
+
+avl_tree = AVLTree()
+"""
+avl_tree.insert(30)
+avl_tree.insert(60)
+avl_tree.insert(50)
+avl_tree.insert(80)
+avl_tree.insert(20)
+avl_tree.insert(200)
+
+avl_tree.traverse()
+
+
+avl_tree.delete(30)
+avl_tree.delete(20)
+avl_tree.delete(50)
+
+
+avl_tree.traverse()
+"""
+avl_tree.insert(10)
+avl_tree.insert(20)
+avl_tree.insert(5)
+avl_tree.insert(4)
+avl_tree.insert(15)
+
+avl_tree.traverse()
+
+avl_tree.delete(5)
+avl_tree.delete(4)
+
+avl_tree.traverse()
